05fdfb456aad560008fa09ed768b19d687ad2eec
dwojtaszek <dwojtaszek@gmail.com>
2026-05-08 08:11:57 +0200

    refactor: consolidate 6 load-file writer subclasses into 2 (Dat + Opt)
    Closes #224

---
+++ src/LoadFiles/WriterMode.cs
@@ -0,0 +1,11 @@
+namespace Zipper.LoadFiles;
+
+/// <summary>
+/// Identifies the output mode for DAT and OPT load file writers.
+/// </summary>
+internal enum WriterMode
+{
+    Standard,
+    LoadfileOnly,
+    ProductionSet,
+}
